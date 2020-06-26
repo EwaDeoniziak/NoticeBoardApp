2ea96081ca0bf57dc5c31221baa51aa495e7ef29
Ewa Deoniziak <deoniziakewa@gmail.com>
2020-06-26 19:01:04 +0200

    poprawki5

--- TablicaOgloszen/src/app/shared/pipes/search-by-location.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SearchByLocationPipe } from './search-by-location.pipe';
-
-describe('SearchByLocationPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SearchByLocationPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
